f72ce20eacf0fe4ab135cdcc90f008dfc2f1b484
unknown <adi120.a@gmail.com>
2019-10-22 19:02:04 +0200

    Add simple Router to project

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import Currencies from './currencies/Currencies'
+import Currencies from './currencies/Currencies';
+import Page from './page/Page';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route    
+  } from "react-router-dom";
 
 class Main extends Component {
     state = {  }
     render() { 
         return (
             <main className='main'>
-                 <Currencies/>
+                <Router>
+                    <Switch>                         
+                        <Route   path='/page/:id'>
+                             <Page/>
+                        </Route>
+                        <Route  path='/'>
+                             <Currencies/>
+                        </Route>
+                    </Switch>
+                </Router>
+                 
             </main>
          );
     }
