be084ddc602446373d19d330a242564ed1c6b1a9
unknown <adi120.a@gmail.com>
2019-11-01 01:21:25 +0100

    Fetch date from API to page componet using axios library;

--- src/components/main/page/Page.js
+++ src/components/main/page/Page.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import './Page.scss'
+import './Page.scss';
+import axios from 'axios';
+
 
 class Page extends Component {
     state = {
-        coin:null
+        
     }
 
-    componentDidMount = () => {
-        console.log(this.props)
+    componentDidMount = async() => {
+        const { id } = this.props.match.params;
+        const response = await axios.get(` https://api.coinlore.com/api/ticker/?id=${id}`)
+        const coin = response.data[0]
+        this.setState({
+            ...coin
+        })        
+      
     }
+
+    componentDidUpdate = () => {
+        console.log(this.state)
+    }
+   
     render() {
         return (
             <section className='page'>
                 <div className='page_logo'>
-
+                   
                 </div>
                 <div className='detalis_logo'>
 
